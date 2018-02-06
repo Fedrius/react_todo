9857e76a873716ef1bd2fc53ab93e218a3aaff99
Paul <paulmlee7@gmail.com>
2018-02-06 12:36:50 -0800

    initial app set up with dummy data

---
+++ src/assets/data/index.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'eat food',
+        complete: false
+    },
+    {
+        title: 'eat some more',
+        complete: false
+    },
+    {
+        title: 'never stop eating',
+        complete: false
+    },
+    {
+        title: 'start eating again',
+        complete: false
+    },
+];
